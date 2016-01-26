99b7de51f7a78d46c91a0903fc8e0954485a5951
Matteo <jiasheng.xia@nest2.com>
2016-01-26 13:53:13 +0100

    remove new_app

--- first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '5d92e76b617f6a25870957cc245abf193e0bfb3bd25c6faab265169d2d8c5be6282e921cb7543e907d48b78d5bc2cd9146c295c03fe9a1669b07d0d32d2de6fc'
--- new_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NewApp::Application.config.secret_token = '561775b490fbb7dc6fc37ce96f231359c37b679e2426e5d55e20db7baa267d88480a330fadcfc531c0fc508b3845a8e6da4dc6c14cef4b199cb0464a4a70188f'
